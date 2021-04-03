0102fb6fa204ce1119b02cf7d20c9a8fbfd5a0ca
rafik mrabet <mrabetr@hotmail.com>
2021-04-03 16:22:26 +0100

    fixed bug in show controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :set_product, only: [:show]
-
   def index
     products = Product.all
     render json: products, status: 200
   end
 
   def show
+    product = Product.find(params[:id])
     if product
       render json: product, status: 200
     else
@@ -24,10 +23,6 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   private
-
-  def set_product
-    @product = Product.find(params[:id])
-  end
 
   def product_params
     params.require(:product).permit(:name, :brand, :price, :description)
